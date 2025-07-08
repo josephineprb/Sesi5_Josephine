d3ec94869e280274363049cc39b890e7ad2d8acb
Josephine Ferdinanda <125816834+josephineprb@users.noreply.github.com>
2025-07-08 23:14:29 +0700

    Add files via upload

---
+++ Sesi 13_Josephine/test/appdemo.test13.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+
+describe('API Demo Testing Sesi 13', () => {
+
+    it('coba test buka aplikasinya', async () => {
+        console.log('Aplikasi Terbuka');
+    });
+});
